3bc87eed559dbc2c5d73528ce2aa282b007a423f
Lazy-Bit-696 <lazyzbit696@gmail.com>
2022-04-25 10:33:25 +0530

    function game done

--- scripty.js
+++ scripty.js
@@ -1,27 +1,22 @@
-// function that returns 'rock' or 'paper' or 'scissor' randomly
+// function that returns 'rock' or 'paper' or 'scissor' randomly. Default return for invalid input is scissor 
 function computerPlay() {
     randomNumber = Math.floor(Math.random() * 3) + 1;
-    // console.log(randomNumber)
     if (randomNumber === 1) {
-        // console.log("Rock")
         return "Rock"
     }
     else if (randomNumber === 2) {
-        // console.log("Paper")
         return "Paper"
     }
     else {
-        // console.log("SCISSOR")
         return "Scissor"
     }
 }
 
+// function that compares selections and returns results of it in a string
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase()
-    console.log("P: ", playerSelection)
-    console.log("C: ",computerSelection)
     if (playerSelection === computerSelection) {
-        return "It's a tie!!!"
+        return "It's a Tie!!!"
     }
     else {
         if (playerSelection === "Rock") {
@@ -51,6 +46,29 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "PAPER";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// asks input 5 times shows results of each rounds and at the end also shows the wins, loses and ties
+function game() {
+    let win = 0
+    let lose = 0
+    let tie = 0
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt("Enter rock, paper or scissor (Default is scissor)");
+        let computerSelection = computerPlay();
+        text = playRound(playerSelection, computerSelection);
+        if ("You win" === text.substring(0,7)) {
+            win += 1
+        }
+        else if ("You Lose" === text.substring(0,8)) {
+            lose += 1
+        }
+        else if ("Tie" === text.substring(7,10)) {
+            tie += 1
+        }
+        console.log(text)
+    }
+    console.log("Your Wins:", win)
+    console.log("Your Lose:", lose)
+    console.log("Your Tie:", tie)
+}
+
+game()
