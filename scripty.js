1e051fd7af3571d9bf504701d179821f8248ccdf
Lazy-Bit-696 <lazyzbit696@gmail.com>
2022-04-25 10:08:10 +0530

    function playRound added

--- scripty.js
+++ scripty.js
@@ -15,3 +15,42 @@ function computerPlay() {
         return "Scissor"
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase()
+    console.log("P: ", playerSelection)
+    console.log("C: ",computerSelection)
+    if (playerSelection === computerSelection) {
+        return "It's a tie!!!"
+    }
+    else {
+        if (playerSelection === "Rock") {
+            if (computerSelection === "Paper") {
+                return "You Lose! Paper beats Rock"
+            }
+            else {
+                return "You win! Rock beats Scissor"
+            }
+        }
+        else if (playerSelection === "Paper") {
+            if (computerSelection === "Scissor") {
+                return "You Lose! Scissor beats Paper"
+            }
+            else {
+                return "You win! Paper beats Rock"
+            }
+        }
+        else {
+            if (computerSelection === "Rock") {
+                return "You Lose! Rock beats Scissor"
+            }
+            else {
+                return "You win! Scissor beats Paper"
+            }
+        }
+    }
+}
+
+const playerSelection = "PAPER";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
